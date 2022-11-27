9d0f9623171a40b54c7526350d9aa35415e505a9
Oleksii Dembnovetskyi <dembnoveckii@gmail.com>
2022-11-27 15:51:32 +0100

    finish task 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,18 +1,18 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
 
 const refs = {
   inputEl: document.querySelector('#datetime-picker'),
   btnEl: document.querySelector('button[data-start]'),
+  daysEl: document.querySelector('span[data-days]'),
+  hoursEl: document.querySelector('span[data-hours]'),
+  minutesEl: document.querySelector('span[data-minutes]'),
+  secondsEl: document.querySelector('span[data-seconds]'),
 };
 
-const timeEl = document.querySelectorAll('.value');
-
-refs.btnEl.addEventListener('click', startTaimer);
-// refs.inputEl.addEventListener('click', qwe);
-
-let interId = null;
-let timerId = null;
+let selectedTime = null;
+refs.btnEl.setAttribute('disabled', true);
 
 const options = {
   enableTime: true,
@@ -22,51 +22,72 @@ const options = {
   onClose(selectedDates) {
     selectedDates[0] <= options.defaultDate
       ? (refs.btnEl.setAttribute('disabled', true),
-        window.alert('Please choose a date in the future'))
+        Notiflix.Report.failure(
+          'ERROR',
+          'Please choose a date in the future',
+          'ok'
+        ))
       : refs.btnEl.removeAttribute('disabled');
     interId = selectedDates[0];
-    convertMs(interId - options.defaultDate);
+    selectedTime = interId - options.defaultDate;
+    // if (selectedTime > 1000) {
+    //   const time = convertMs(selectedTime);
+    //   showDate(time);
+    // }
+    return;
   },
 };
 
+flatpickr(refs.inputEl, options, {});
+
+refs.btnEl.addEventListener('click', () => {
+  timer.start();
+});
+
+const timer = {
+  intevalId: null,
+  start() {
+    this.intevalId = setInterval(() => {
+      const time = convertMs((selectedTime -= 1000));
+      getDate(time);
+      refs.btnEl.setAttribute('disabled', true);
+      if (selectedTime <= 1000) {
+        this.stop();
+      }
+    }, 1000);
+  },
+  stop() {
+    clearInterval(this.intevalId);
+    Notiflix.Report.info('Time is over', 'Please choose a date', 'Ok');
+  },
+};
+
+function pad(value) {
+  return String(value).padStart(2, '0');
+}
+
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
-
-  // Remaining days
-  const days = Math.floor(ms / day);
-  // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-  timerId = { days, hours, minutes, seconds };
+  const days = pad(Math.floor(ms / day));
+  const hours = pad(Math.floor((ms % day) / hour));
+  const minutes = pad(Math.floor(((ms % day) % hour) / minute));
+  const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
+  return { days, hours, minutes, seconds };
 }
 
-convertMs();
-console.log(timerId);
-
-// timerId = setInterval(convertMs, 1000);
-
-function startTaimer(event) {
-  for (const timer of timerId) {
-    timeEl.forEach(item => {
-      item.textContent = timerId[timer];
-    });
-  }
-
-  refs.spanDay.textContent = timerId.days;
-  refs.spanHours.textContent = timerId.hours;
-  refs.spanMin.textContent = timerId.minutes;
-  refs.spanSec.textContent = timerId.seconds;
-  setInterval(() => {
-    refs.spanSec.textContent - 1;
-  }, 1000);
+function getDate({ days, hours, minutes, seconds }) {
+  refs.daysEl.textContent = days;
+  refs.hoursEl.textContent = hours;
+  refs.minutesEl.textContent = minutes;
+  refs.secondsEl.textContent = seconds;
 }
 
-flatpickr(refs.inputEl, options, {});
+// function showDate({ days, hours, minutes, seconds }) {
+//   refs.daysEl.textContent = days;
+//   refs.hoursEl.textContent = hours;
+//   refs.minutesEl.textContent = minutes;
+//   refs.secondsEl.textContent = seconds;
+// }
